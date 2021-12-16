cf92aafd8f7825447dbdaefcdadc0111026699f1
zensty <96226315+zensty@users.noreply.github.com>
2021-12-16 16:02:09 +0900

    Add files via upload

---
+++ ZSTToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./token/ERC20/Ownable.sol";
+import "./token/ERC20/ERC20.sol";
+
+contract ZSTToken is Ownable, ERC20{
+
+    uint256 private _maxtotalSupply = 1000000000 * 10 ** decimals();
+
+    constructor () Ownable() ERC20("ZENSTY Token", "ZST"){
+        _mint(msg.sender,  _maxtotalSupply);
+    }
+
+    function burn(uint256 _value) external onlyOwner {
+        _burn(_msgSender(), _value);
+    }
+
+}
